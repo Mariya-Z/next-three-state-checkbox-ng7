c8b7776475d8f4f87a09dc1e0164c12797d60593
Mariya-Z <zubkovaa.mariya@yandex.ru>
2019-05-21 17:15:41 +0300

    add web component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
-import { AppComponent } from './app.component';
+import { NextThreeStateCheckboxComponent } from './next-three-state-checkbox/next-three-state-checkbox.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [NextThreeStateCheckboxComponent],
+  imports: [BrowserModule],
+  entryComponents: [NextThreeStateCheckboxComponent],
+  bootstrap: [],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(private injector: Injector) {
+    const nextCheckbox = createCustomElement(NextThreeStateCheckboxComponent, {
+      injector,
+    });
+    customElements.define('next-checkbox', nextCheckbox);
+  }
+  ngDoBootstrap() {}
+}
